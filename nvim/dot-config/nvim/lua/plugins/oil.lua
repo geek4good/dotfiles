e42bad5d3b57713ea5044253a2a09298a927eb4f
Lucas Mbiwe <lucas@geek4good.com>
2026-04-14 12:07:44 +0700

    Replace NeoTree with Oil as default file explorer

--- nvim/dot-config/nvim/lua/plugins/oil.lua
+++ nvim/dot-config/nvim/lua/plugins/oil.lua
@@ -1,8 +1,15 @@
 return {
+  -- Disable NeoTree
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+
+  -- Use Oil as the default file explorer
   {
-    'stevearc/oil.nvim', version = '0.10',
-    opts = {},
-    -- Optional dependencies
+    "stevearc/oil.nvim",
+    version = "0.10",
+    lazy = false,
+    opts = {
+      default_file_explorer = true,
+    },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     keys = {
       { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
