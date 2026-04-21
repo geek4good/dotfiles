012db6a3bb42377926171ce7909bb5086e936be3
Lucas Mbiwe <lucas@geek4good.com>
2026-04-21 09:57:53 +0700

    fix(nvim): set C-hjkl in user keymaps to beat LazyVim defaults

--- nvim/dot-config/nvim/lua/config/keymaps.lua
+++ nvim/dot-config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,9 @@
 -- vim.keymap.del("n", "<leader>gG")
 -- vim.keymap.del("n", "<leader>gs")
 vim.keymap.set("n", "<leader>fs", "<cmd>write<CR>", { desc = "Save File" })
+
+vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { desc = "Navigate Left (tmux)" })
+vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { desc = "Navigate Down (tmux)" })
+vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { desc = "Navigate Up (tmux)" })
+vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { desc = "Navigate Right (tmux)" })
+vim.keymap.set("n", "<C-\\>", "<cmd>TmuxNavigatePrevious<CR>", { desc = "Navigate Previous (tmux)" })
--- nvim/dot-config/nvim/lua/plugins/tmux-navigator.lua
+++ nvim/dot-config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,20 +1,7 @@
 return {
   {
     "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-    },
-    keys = {
-      { "<C-h>", "<cmd><C-U>TmuxNavigateLeft<CR>", desc = "Navigate Left (tmux)" },
-      { "<C-j>", "<cmd><C-U>TmuxNavigateDown<CR>", desc = "Navigate Down (tmux)" },
-      { "<C-k>", "<cmd><C-U>TmuxNavigateUp<CR>", desc = "Navigate Up (tmux)" },
-      { "<C-l>", "<cmd><C-U>TmuxNavigateRight<CR>", desc = "Navigate Right (tmux)" },
-      { "<C-\\>", "<cmd><C-U>TmuxNavigatePrevious<CR>", desc = "Navigate Previous (tmux)" },
-    },
+    lazy = false,
     init = function()
       vim.g.tmux_navigator_no_mappings = 1
     end,
