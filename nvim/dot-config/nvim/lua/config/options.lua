d75f666d2516c798cbaef039c5af8f9e04b67498
Lucas Mbiwe <lucas@geek4good.com>
2026-04-27 09:59:26 +0700

    feat(nvim): configure neovide font (Iosevka NF:h20) and simplify fallback colorscheme

--- nvim/dot-config/nvim/lua/config/options.lua
+++ nvim/dot-config/nvim/lua/config/options.lua
@@ -2,3 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.lazyvim_ruby_formatter = "standardrb"
+
+if vim.g.neovide then
+  vim.o.guifont = "Iosevka NF:h20"
+end
