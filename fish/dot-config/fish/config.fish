bb8e34a05ea45f1bb5bc66c0f08dcb1dba29ba98
Lucas Mbiwe <lucas@geek4good.com>
2026-04-29 23:35:24 +0700

    chore(fish): update abbreviations
    - Add ash abbreviation for autossh
    - Remove oc and s abbreviations

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -93,6 +93,5 @@ abbr -a gclean 'git clean -fd'
 
 # Abbreviations — work
 abbr -a auto 'npx autoskills'
+abbr -a ash "autossh -M 0 -q"
 abbr -a cf 'v ~/.config/fish/config.fish'
-abbr -a oc 'ocx oc -p ws'
-abbr -a s 'sesh connect "$(sesh list -ti | gum filter --limit 1 --placeholder \'Pick a sesh\' --prompt=\'⚡\')"'
