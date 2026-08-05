3cef69420bac1a64a6281d4edfc43bab28709b3f
Lucas Mbiwe <lucas@geek4good.com>
2026-08-05 22:40:13 +0100

    fix(nvim): run vim-test via neovim terminal strategy
    Drop the vimux dependency and use the built-in 'neovim' strategy
    (for the :terminal buffer) since tmux is no longer used, and move the
    strategy assignment into init where the variable is valid.

--- nvim/dot-config/nvim/lua/plugins/vim-test.lua
+++ nvim/dot-config/nvim/lua/plugins/vim-test.lua
@@ -1,17 +1,18 @@
 return {
   "vim-test/vim-test",
-  dependencies = {
-    "preservim/vimux",
-  },
-  vim.keymap.set("n", "<leader>tt", "<cmd>TestNearest<CR>", { desc = "Run nearest test" }),
-  vim.keymap.set("n", "<leader>tT", "<cmd>TestFile<CR>", { desc = "Run whole file" }),
-  vim.keymap.set("n", "<leader>ta", "<cmd>TestSuite<CR>", { desc = "Run all tests" }),
-  vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<CR>", { desc = "Re-run last test(s)" }),
-  vim.keymap.set("n", "<leader>tg", "<cmd>TestVisit<CR>", { desc = "Visit last test file" }),
-  vim.cmd("let test#strategy = 'vimux'"),
   init = function()
+    -- Run tests in a :terminal buffer (no tmux needed)
+    vim.g["test#strategy"] = "neovim"
+
     require("which-key").add({
       { "<leader>t", group = "test" },
     })
   end,
+  keys = {
+    { "<leader>tt", "<cmd>TestNearest<CR>", desc = "Run nearest test" },
+    { "<leader>tT", "<cmd>TestFile<CR>", desc = "Run whole file" },
+    { "<leader>ta", "<cmd>TestSuite<CR>", desc = "Run all tests" },
+    { "<leader>tl", "<cmd>TestLast<CR>", desc = "Re-run last test(s)" },
+    { "<leader>tg", "<cmd>TestVisit<CR>", desc = "Visit last test file" },
+  },
 }
